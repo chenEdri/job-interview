e5cac5220bf274ed82c8695fd439090751b9130f
chen edri <chenedri22@gmail.com>
2021-03-21 22:38:21 +0200

    Updates

---
+++ precache-manifest.6aa30c26198ee99e9980d4ba73d23af8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8163bdf18c54a7f2296de30e05f6bdd8",
+    "url": "/job-interview/index.html"
+  },
+  {
+    "revision": "7a79f5080dbfcab4b239",
+    "url": "/job-interview/static/css/main.8388e140.chunk.css"
+  },
+  {
+    "revision": "ae14d6cdc0434d924471",
+    "url": "/job-interview/static/js/2.0afa2c81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/job-interview/static/js/2.0afa2c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a79f5080dbfcab4b239",
+    "url": "/job-interview/static/js/main.5239ab5e.chunk.js"
+  },
+  {
+    "revision": "eebef253cb97436d49dd",
+    "url": "/job-interview/static/js/runtime-main.6c7393e2.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/job-interview/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/job-interview/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/job-interview/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/job-interview/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
